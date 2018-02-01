67d5280f0f0844d38611c88616bf1b27f0d96a86
ksenia007 <ks352@duke.edu>
2018-02-01 10:07:00 -0500

    Added comment and cleaned a bit

--- sketch.js
+++ sketch.js
@@ -6,39 +6,32 @@ function setup(){
     createCanvas(500, 800);
 
     
-    img.resize(0,height);
-    img.filter(BLUR,1);
+    img.resize(width,0); //resized the image to fit, 0 means that there will be no contraint on this parameter (no distortion)
+    img.filter(BLUR,1); //blur the image to alleviate the effect of the noise
 
-    //create canvas
-
-    // change the frame rate to make slower
+    // set the frame rate 
     frameRate(30);
-    //load image
     
-    //img.loadPixels();
     background(255);
 }
 
 function draw(){ 
-    
-    
-
-    // Set the background to black and turn off the fill color
-    //image(img,0,0);
-    //var x=floor(width*noise(0.1*frameCount));
+    //get ranfom x and y locations
     var x=floor(random(width));
     var y=floor(random(height));
-    print(x,y);
+    //get the color of the pixel at the selected location
     var col=color(img.get(x,y));
-    print(col);
-    tr1=new triangleObj(x,y, col);
+    //create new rectanle
+    tr1=new rectangleObj(x,y, col);
+    //call the function that actually draws it
     tr1.generate();
-    print(frameCount);
 }
 
-function triangleObj(x,y, col){
-    this.x=x;
+function rectangleObj(x,y, col){
+    //set the center
+    this.x=x; 
     this.y=y;
+    //set the size depending on the frame count
     if (frameCount<100){
         this.dist=height/5;
     }
@@ -57,19 +50,15 @@ function triangleObj(x,y, col){
     else{
         this.dist=height/70;
     }
-    this.scale=100/frameCount;
+    //set the color
     this.col=col;
     this.generate=function(){
         push();
         noStroke();
-        var r1=floor(random(this.dist));
-        var x1=constrain(this.x+r1,0, width);
-        var y2=constrain(this.y+r1,0, height);
-        var x3=constrain(this.x-r1,0, width);
-        col.setAlpha(200);
-        fill(this.col);
-        rect(this.x, this.y, this.dist, this.dist);
-        //triangle(x1, this.y, this.x, y2, x3, this.y);
+        rectMode(CENTER);
+        col.setAlpha(200); //change alpha for the color to make transparent
+        fill(this.col); //set colot
+        rect(this.x, this.y, this.dist, this.dist); //draw
         pop();
     }
 }
